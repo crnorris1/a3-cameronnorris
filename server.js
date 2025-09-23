8bdcdfc36c68fe1ffc77da5b3e0709a8d923a09f
crnorris1 <crnorris@wpi.edu>
2025-09-23 11:52:16 -0400

    Modification so that it can work with github pages

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express    = require('express'),
 
 //app.use(express.static(path.join(__dirname, "public")));
 app.use( express.static( 'views'  ) )
-app.use( express.static( 'public'  ) )
+app.use( express.static( 'docs'  ) )
 
 app.use(express.urlencoded({ extended: true }));
 
